af671315b5b33c8a4f0af2495da3a10f154e96be
Somnath Paul <125501490+somnathpaul99@users.noreply.github.com>
2023-05-22 02:12:26 +0530

    refactoring javaScript code

--- App.js
+++ App.js
@@ -1,77 +1,103 @@
-function savedButton() {
-    // console.log("Working")
-    let input1 = document.querySelector(".inputBox1").innerHTML;
-    let input2 = document.querySelector(".inputBox2").innerHTML;
+//getting resulatSection from the html and also it placed in global scope because it will used in below some function
+const resultSection = document.getElementsByClassName("output")[0];
 
-    localStorage.setItem(input1, input2);
-    console.log(localStorage.getItem(input2));
-}
+//placed this variable in global scope because it will used in below some function
+//and this variable gets value from SearchButton function
+let result;
+let operation ;
+let expression ; 
 
-let count = 0;
-function historyButton() {
+//when click SearchButton icon this function will fetch from Api and also append the Ans to the UI
+async function SearchButton() {
     // console.log("working");
-    count++;
-    if (count % 2 == 1) {
-        document.querySelector(".historyData").style.display = "block";
-        let data = document.querySelector(".historyData");
-
-        for (savedCalculations in localStorage) {
-            let ans = localStorage.getItem(savedCalculations);
-            if (ans != null) {
-                // console.log(ans);
-                let h3 = document.createElement("h3");
-                h3.setAttribute('class', 'remove');
-                h3.innerHTML = `${savedCalculations} => result = ${ans}`;
-                console.log(h3);
-                data.appendChild(h3);
-            }
-        }
-    } else {
-        document.querySelector(".historyData").style.display = "none";
-        let rem = document.querySelector(".remove");
-        for (var i = 0; i < rem.clientHeight; i++) {
-            rem[i].remove();
-        }
-    }
-}
-
-function SearchButton() {
-    // console.log("working");
-    let problem = document.getElementById("problem-section").value;
-    let category = document.getElementById("category-section").value;
+    let problem = document.getElementById("problemSection").value;
+    let category = document.getElementById("categorySection").value;
     const urlEncodedInput = encodeURIComponent(problem);
-    let fetchApi = fetch(`https://newton.vercel.app/api/v2/${category}/${urlEncodedInput}`)
-    // console.log(fetchApi)
-    fetchApi.then((response) => {
-        // console.log(response.status)
-        // console.log(response.ok)
-        return response.json();
-    }).then((value) => {
-        // console.log(value)
-        let input2 = value.result;
-        document.querySelector(".inputBox2").innerHTML = input2;
-    })
+    const fetchApi = await fetch(`https://newton.vercel.app/api/v2/${category}/${urlEncodedInput}`)
+    const data = await fetchApi.json();
+    //  console.log(data)
+     result = data.result;
+     operation = data.operation;
+     expression = data.expression;
+  
+    const div = document.createElement("div");
+    div.className = "output";
 
-    document.querySelector(".inputBox1").innerHTML = category + " :" + problem;
-    let key = document.querySelector(".inputBox1").value;
-    let ans = document.querySelector(".inputBox2").innerHTML;
-    console.log(ans)
-    localStorage.setItem(key, ans);
+    div.innerHTML = `
+    <p class="key">${operation}: ${expression}</p>
+    <p class="ans">Ans: ${result}</p>
+  `;
+
+  resultSection.appendChild(div);
+}
+
+//when click savedButton this function will saved all the details to the localStorage
+function savedButton(){
+ localStorage.setItem(`${operation}: ${expression}`, result);
+ generateToastMessage("Answer Saved");
+
+}
+
+//when click historyButton this function will show all saved history to the UI
+function historyButton() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    let p = document.createElement("p");
+    let ans = `${key} => Ans: ${value}`;
+    p.innerText = ans;
+    p.className = "history-storage";
+
+    // Create a delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete-button";
+
+    // Append delete button to the <p> element
+    p.appendChild(deleteButton);
+
+    resultSection.appendChild(p);
+
+     //when click the button this ans will remove ans from local storage
+    deleteButton.addEventListener('click', (event) => {
+      event.stopPropagation(); // Prevent the double-click event from firing
+      resultSection.removeChild(p);
+      // Remove item from localStorage
+      localStorage.removeItem(key);
+      generateToastMessage("This Answer Deleted");
+    });
+  }
+  
 }
 
 
+//when click delete button once it will clear display
 function deleteBtn() {
-    // console.log(working)
-    let key = document.querySelector(".inputBox1").value;
-    localStorage.removeItem(key);
-    document.querySelector(".inputBox1").value = "";
-    document.querySelector(".inputBox2").textContent = "";
+  resultSection.innerHTML = ""; // Clear displayed history
+}
 
-    // clear history data
-    document.querySelector(".historyData").style.display = "none";
-    localStorage.clear();
-    let rem = document.querySelectorAll(".remove");
-    for (let i = 0; i < rem.length; i++) {
-        rem[i].remove();
-    }
+//getting delete button from html
+const deleteHistory = document.getElementById("deleteBtn");
+
+//adding an event Listener when dubble click the delete icon it will clear all localStorage history
+deleteHistory.addEventListener("dblclick", ()=>{
+   localStorage.clear();
+   generateToastMessage("Clear All Saved Answer")
+})
+
+//creating a function for toast messege
+const container = document.getElementById("container")
+function generateToastMessage(string) {
+  const div = document.createElement("div");
+  div.innerText = string;
+  div.className = 'toast-message toast-message-slide-in';
+  div.addEventListener('click', () => {
+      div.classList.remove('toast-message-slide-in')
+      div.classList.add('toast-message-slide-out')
+      div.addEventListener('animationend', () => {
+          div.remove();
+      })
+  })
+  container.appendChild(div);
 }
